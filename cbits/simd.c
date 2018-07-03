b9f49542b08dd7af2e87fa5267782d5d833281c3
John Ky <newhoggy@gmail.com>
2018-07-03 20:32:25 +1000

    Define build_ibs C function with sample main

--- cbits/simd.c
+++ cbits/simd.c
@@ -1,61 +1,108 @@
 #include "simd.h"
 
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <mmintrin.h>
-#include <unistd.h>
 #include <immintrin.h>
+#include <mmintrin.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-typedef uint8_t v16si __attribute__ ((vector_size (16)));
+typedef uint8_t v32si __attribute__ ((vector_size (32)));
 
-void moo ()
-{
-  printf("Hello c-bits\n");
+uint64_t build_ibs(
+    char delimiter,
+    uint64_t quote_count,
+    char *in,
+    size_t len,
+    uint64_t *out_markers,
+    uint64_t *out_newlines) {
+  if (len % 64 != 0) {
+    printf("Invalid length: %zu\n", len);
+    return 0xffffffff;
+  }
+
+  v32si ws_newlines   = _mm256_set1_epi8('\n');
+  v32si ws_delimiters = _mm256_set1_epi8(delimiter);
+  v32si ws_quotes     = _mm256_set1_epi8('"');
+
+  for (size_t i = 0; i < len / 64; ++i) {
+    v32si lo_s = *(v32si*)(in + (i * 64));
+    v32si lo_rb_newlines    = _mm256_cmpeq_epi8(lo_s, ws_newlines);
+    v32si lo_rb_delimiters  = _mm256_cmpeq_epi8(lo_s, ws_delimiters);
+    v32si lo_rb_quotes      = _mm256_cmpeq_epi8(lo_s, ws_quotes);
+
+    uint32_t lo_rib_newlines      = (uint32_t)_mm256_movemask_epi8(lo_rb_newlines);
+    uint32_t lo_rib_delimiters    = (uint32_t)_mm256_movemask_epi8(lo_rb_delimiters);
+    uint32_t lo_rib_quotes        = (uint32_t)_mm256_movemask_epi8(lo_rb_quotes);
+    uint64_t lo_rib_open_quotes   = _pdep_u64(0x5555555555555555L << (quote_count & 1), lo_rib_quotes);
+    uint64_t lo_rib_close_quotes  = _pdep_u64(0xaaaaaaaaaaaaaaaaL >> (quote_count & 1), lo_rib_quotes);
+
+    quote_count += _popcnt64(lo_rib_quotes);
+
+    v32si hi_s = *(v32si*)(in + (i * 64) + 32);
+    v32si hi_rb_newlines    = _mm256_cmpeq_epi8(hi_s, ws_newlines);
+    v32si hi_rb_delimiters  = _mm256_cmpeq_epi8(hi_s, ws_delimiters);
+    v32si hi_rb_quotes      = _mm256_cmpeq_epi8(hi_s, ws_quotes);
+
+    uint32_t hi_rib_newlines      = (uint32_t)_mm256_movemask_epi8(hi_rb_newlines);
+    uint32_t hi_rib_delimiters    = (uint32_t)_mm256_movemask_epi8(hi_rb_delimiters);
+    uint32_t hi_rib_quotes        = (uint32_t)_mm256_movemask_epi8(hi_rb_quotes);
+    uint64_t hi_rib_open_quotes   = _pdep_u64(0x5555555555555555L << (quote_count & 1), hi_rib_quotes);
+    uint64_t hi_rib_close_quotes  = _pdep_u64(0xaaaaaaaaaaaaaaaaL >> (quote_count & 1), hi_rib_quotes);
+
+    quote_count += _popcnt64(hi_rib_quotes);
+
+    uint64_t rib_newlines      = ((uint64_t)hi_rib_newlines     << 32) | (uint64_t)lo_rib_newlines;
+    uint64_t rib_delimiters    = ((uint64_t)hi_rib_delimiters   << 32) | (uint64_t)lo_rib_delimiters;
+    uint64_t rib_open_quotes   = ((uint64_t)hi_rib_open_quotes  << 32) | (uint64_t)lo_rib_open_quotes;
+    uint64_t rib_close_quotes  = ((uint64_t)hi_rib_close_quotes << 32) | (uint64_t)lo_rib_close_quotes;
+    uint64_t rib_markers       = rib_newlines | rib_delimiters;
+
+    uint64_t quote_mask = (~rib_close_quotes) + rib_open_quotes;
+
+    out_newlines[i] = rib_newlines & quote_mask;
+    out_markers[i]  = rib_markers  & quote_mask;
+  }
+
+  return quote_count;
 }
 
-void run(char *in, size_t len, unsigned char *tBytes, char *out) {
-  unsigned char a, b, c, d, e, f, g;
-  int i, j;
-  v16si s = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-  v16si s0, s1, s2, s3, s4, s5;
-  v16si t[256];
+int example_main()
+{
+  FILE *in = fopen("example.csv", "r");
+  FILE *out_newlines = fopen("example.csv.newlines.idx", "w");
+  FILE *out_markers = fopen("example.csv.markers.idx", "w");
 
-  for (i = 0; i < 256; i++) {
-    for (j = 0; j < 16; j++) {
-      t[i][j] = tBytes[16 * i + j];
+  if (!in || !out_newlines || !out_markers) {
+    return 1;
+  }
+
+  char buffer[4096];
+  uint64_t buffer_markers[64];
+  uint64_t buffer_newlines[64];
+  size_t quote_count = 0;
+
+  size_t bytes_read = fread(buffer, 1, 4096, in);
+
+  while (bytes_read > 0) {
+    if (bytes_read < 4096) {
+      memset(buffer + bytes_read, 0, 4096 - bytes_read);
     }
+
+    quote_count = build_ibs(
+      ',',
+      quote_count,
+      buffer,
+      4096,
+      buffer_markers,
+      buffer_newlines);
+
+    fwrite(buffer_newlines, sizeof(uint64_t), 64, out_newlines);
+    fwrite(buffer_markers,  sizeof(uint64_t), 64, out_markers);
+
+    bytes_read = fread(buffer, 1, 4096, in);
   }
 
-  for (i = 0; i + 6 < len; i += 7) {
-    // These can be run in parallel
-    a = in[i    ];
-    b = in[i + 1];
-    c = in[i + 2];
-    d = in[i + 3];
-    e = in[i + 4];
-    f = in[i + 5];
-    g = in[i + 6];
-
-    // These can be run in parallel
-    s0 = _mm_shuffle_epi8(t[a], s   );
-    s1 = _mm_shuffle_epi8(t[c], t[b]);
-    s2 = _mm_shuffle_epi8(t[e], t[d]);
-    s3 = _mm_shuffle_epi8(t[g], t[f]);
-
-    // These can be run in parallel
-    s4 = _mm_shuffle_epi8(s1  , s0  );
-    s5 = _mm_shuffle_epi8(s3  , s2  );
-
-    s  = _mm_shuffle_epi8(s5  , s4  );
-  }
-
-  for (j = i; j < len; j++) {
-    a = in[j];
-    s = _mm_shuffle_epi8(t[a], s);
-  }
-
-  for (i = 0; i < 16; i++) {
-    out[i] = s[i];
-  }
+  return 0;
 }
