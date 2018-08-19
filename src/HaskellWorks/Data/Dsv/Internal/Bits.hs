3c036bf1635381272dbc6896e7f999818edc1b99
John Ky <newhoggy@gmail.com>
2018-08-20 00:47:59 +1000

    Simplify and/or functions

--- src/HaskellWorks/Data/Dsv/Internal/Bits.hs
+++ src/HaskellWorks/Data/Dsv/Internal/Bits.hs
@@ -15,35 +15,29 @@ testWord8s w =  let w8s = w
 {-# INLINE testWord8s #-}
 
 zipOr :: DVS.Vector Word64 -> DVS.Vector Word64 -> DVS.Vector Word64
-zipOr as bs = DVS.constructN (DVS.length as `max` DVS.length bs) go
-  where go :: DVS.Vector Word64 -> Word64
-        go u =
-          let ui = DVS.length u
-          in if ui < DVS.length as && ui < DVS.length bs
-            then DVS.unsafeIndex as ui .|. DVS.unsafeIndex bs ui
-            else error "Different sized vectors"
+zipOr as bs = if DVS.length as == DVS.length bs
+  then DVS.constructN len go
+  else error "Different sized vectors"
+  where len = DVS.length as `min` DVS.length bs
+        go :: DVS.Vector Word64 -> Word64
+        go u = let ui = DVS.length u in DVS.unsafeIndex as ui .|. DVS.unsafeIndex bs ui
 {-# INLINE zipOr #-}
 
 zip2Or :: [DVS.Vector Word64] -> [DVS.Vector Word64] -> [DVS.Vector Word64]
 zip2Or (a:as) (b:bs) = zipOr a b:zip2Or as bs
-zip2Or (a:as) []     = a:zip2Or as []
-zip2Or []     (b:bs) = b:zip2Or [] bs
-zip2Or []     []     = []
+zip2Or _     _       = []
 {-# INLINE zip2Or #-}
 
 zipAnd :: DVS.Vector Word64 -> DVS.Vector Word64 -> DVS.Vector Word64
-zipAnd as bs = DVS.constructN (DVS.length as `max` DVS.length bs) go
-  where go :: DVS.Vector Word64 -> Word64
-        go u =
-          let ui = DVS.length u
-          in if ui < DVS.length as && ui < DVS.length bs
-            then DVS.unsafeIndex as ui .&. DVS.unsafeIndex bs ui
-            else error "Different sized vectors"
+zipAnd as bs = if DVS.length as == DVS.length bs
+  then DVS.constructN len go
+  else error "Different sized vectors"
+  where len = DVS.length as `min` DVS.length bs
+        go :: DVS.Vector Word64 -> Word64
+        go u = let ui = DVS.length u in DVS.unsafeIndex as ui .&. DVS.unsafeIndex bs ui
 {-# INLINE zipAnd #-}
 
 zip2And :: [DVS.Vector Word64] -> [DVS.Vector Word64] -> [DVS.Vector Word64]
 zip2And (a:as) (b:bs) = zipAnd a b:zip2And as bs
-zip2And (a:as) []     = a:zip2And as []
-zip2And []     (b:bs) = b:zip2And [] bs
-zip2And []     []     = []
+zip2And _      _      = []
 {-# INLINE zip2And #-}
