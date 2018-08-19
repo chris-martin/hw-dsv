6760c72e2257f68ba0eef3518e9dd0c10c793610
John Ky <newhoggy@gmail.com>
2018-08-19 23:21:43 +1000

    Stuff

--- src/HaskellWorks/Data/Dsv/Lazy/Cursor.hs
+++ src/HaskellWorks/Data/Dsv/Lazy/Cursor.hs
@@ -37,9 +37,9 @@ makeCursor delimiter cs = DsvCursor
   , dsvCursorNewlines  = nls
   , dsvCursorPosition  = 0
   }
-  where ibq = asVector64 <$> BS.rechunk 512 (BS.toByteStrings (SIMD.cmpEqWord8s C.doubleQuote cs))
-        ibn = asVector64 <$> BS.rechunk 512 (BS.toByteStrings (SIMD.cmpEqWord8s C.newline     cs))
-        ibd = asVector64 <$> BS.rechunk 512 (BS.toByteStrings (SIMD.cmpEqWord8s delimiter     cs))
+  where ibq = asVector64 <$> BS.rechunk 64 (BS.toByteStrings (SIMD.cmpEqWord8s C.doubleQuote cs))
+        ibn = asVector64 <$> BS.rechunk 64 (BS.toByteStrings (SIMD.cmpEqWord8s C.newline     cs))
+        ibd = asVector64 <$> BS.rechunk 64 (BS.toByteStrings (SIMD.cmpEqWord8s delimiter     cs))
         pcq = makeCummulativePopCount ibq
         ibr = zip2Or ibn ibd
         qm  = makeQuoteMask ibq pcq
